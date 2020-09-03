44e3db0704dc1b6bbec8b11e6c0af7a501675358
steven vuong <stevenvuong@stevens-MacBook-Pro.local>
2020-09-03 19:28:29 +0100

    follow different guide

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-import os
-with open(os.path.join('./fast-style-transfer','Procfile'), "w") as file1:
-    toFile = 'web: sh setup.sh && streamlit run streamlit.py'
-    
-file1.write(toFile)
+web: sh setup.sh && streamlit run streamlit.py
--- setup.sh
+++ setup.sh
@@ -1,17 +1,3 @@
-#! /bin/bash
-
-mkdir models
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B9jhaT37ydSyQU1sYW02Sm9kV3c' -O './models/la_muse.ckpt'
-wget --no-check-certificate 'hhttps://docs.google.com/uc?export=0B9jhaT37ydSyaEJlSFlIeUxweGs' -O './models/rain_princess.ckpt'
-wget --no-check-certificate 'https://docs.google.com/uc?export=0B9jhaT37ydSyZ0RyTGU0Q2xiU28' -O './models/scream.ckpt'
-wget --no-check-certificate 'https://docs.google.com/uc?export=0B9jhaT37ydSyb0NuYmk2ZEpOR0E' -O './models/udnie.ckpt'
-wget --no-check-certificate 'https://docs.google.com/uc?export=folders/0B9jhaT37ydSyVGk0TC10bDF1S28' -O './models/wave.ckpt'
-wget --no-check-certificate 'https://docs.google.com/uc?export=folders/0B9jhaT37ydSySjNrM3J5N2gweVk' -O './models/wreck.ckpt'
-
-mkdir data
-mkdir data/input 
-mkdir data/output
-
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
@@ -23,3 +9,15 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+# mkdir models
+# wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B9jhaT37ydSyQU1sYW02Sm9kV3c' -O './models/la_muse.ckpt'
+# wget --no-check-certificate 'hhttps://docs.google.com/uc?export=0B9jhaT37ydSyaEJlSFlIeUxweGs' -O './models/rain_princess.ckpt'
+# wget --no-check-certificate 'https://docs.google.com/uc?export=0B9jhaT37ydSyZ0RyTGU0Q2xiU28' -O './models/scream.ckpt'
+# wget --no-check-certificate 'https://docs.google.com/uc?export=0B9jhaT37ydSyb0NuYmk2ZEpOR0E' -O './models/udnie.ckpt'
+# wget --no-check-certificate 'https://docs.google.com/uc?export=folders/0B9jhaT37ydSyVGk0TC10bDF1S28' -O './models/wave.ckpt'
+# wget --no-check-certificate 'https://docs.google.com/uc?export=folders/0B9jhaT37ydSySjNrM3J5N2gweVk' -O './models/wreck.ckpt'
+
+# mkdir data
+# mkdir data/input 
+# mkdir data/output
