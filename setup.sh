f77993656267dd2e389da0ef5b562b31eedd7440
steven vuong <stevenvuong@stevens-MacBook-Pro.local>
2020-09-03 06:33:26 +0100

    make it better

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+mkdir models
+# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/la_muse.ckpt' -O 'la_muse.ckpt'
+# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/rain_princess.ckpt' -O FILENAME
+# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/scream.ckpt' -O FILENAME
+# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/udnie.ckpt' -O FILENAME
+# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/wreck.ckpt' -O FILENAME
+
 mkdir data
 mkdir data/input 
 mkdir data/output
