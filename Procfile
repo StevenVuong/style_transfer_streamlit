392d55ad267f9f5e395d7d806715bb2c4fba3008
steven vuong <stevenvuong@stevens-MacBook-Pro.local>
2020-09-03 06:12:27 +0100

    setting up heroku

---
+++ Procfile
@@ -0,0 +1,5 @@
+import os
+with open(os.path.join('./fast-style-transfer','Procfile'), "w") as file1:
+    toFile = 'web: sh setup.sh && streamlit run streamlit.py'
+    
+file1.write(toFile)
--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
 #! /bin/bash
 
 mkdir data
-cd data
-wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
-mkdir bin
-wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-unzip -q train2014.zip
+mkdir data/input 
+mkdir data/output
+
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"your@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
+# cd data
+# wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+# mkdir bin
+# wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+# unzip -q train2014.zip
