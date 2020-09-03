c79813e52ffd7d3e6df534775d43fc58964a1675
steven vuong <stevenvuong@stevens-MacBook-Pro.local>
2020-09-03 06:43:27 +0100

    include downloads to setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 mkdir models
-# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/la_muse.ckpt' -O 'la_muse.ckpt'
-# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/rain_princess.ckpt' -O FILENAME
-# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/scream.ckpt' -O FILENAME
-# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/udnie.ckpt' -O FILENAME
-# wget --no-check-certificate 'https://drive.google.com/drive/folders/0B9jhaT37ydSyRk9UX0wwX3BpMzQ/wreck.ckpt' -O FILENAME
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B9jhaT37ydSyQU1sYW02Sm9kV3c' -O './models/la_muse.ckpt'
+wget --no-check-certificate 'hhttps://docs.google.com/uc?export=0B9jhaT37ydSyaEJlSFlIeUxweGs' -O './models/rain_princess.ckpt'
+wget --no-check-certificate 'https://docs.google.com/uc?export=0B9jhaT37ydSyZ0RyTGU0Q2xiU28' -O './models/scream.ckpt'
+wget --no-check-certificate 'https://docs.google.com/uc?export=0B9jhaT37ydSyb0NuYmk2ZEpOR0E' -O './models/udnie.ckpt'
+wget --no-check-certificate 'https://docs.google.com/uc?export=folders/0B9jhaT37ydSyVGk0TC10bDF1S28' -O './models/wave.ckpt'
+wget --no-check-certificate 'https://docs.google.com/uc?export=folders/0B9jhaT37ydSySjNrM3J5N2gweVk' -O './models/wreck.ckpt'
 
 mkdir data
 mkdir data/input 
