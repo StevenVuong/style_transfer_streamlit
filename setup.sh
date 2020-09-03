a848a8d0848deb9bb1efc0ba72cfd8aefcb0e177
steven vuong <stevenvuong@stevens-MacBook-Pro.local>
2020-09-03 18:32:17 +0100

    modify setup

--- setup.sh
+++ setup.sh
@@ -23,9 +23,3 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
-
-# cd data
-# wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
-# mkdir bin
-# wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-# unzip -q train2014.zip
