890062712d0a69801486fd0bdbf135fdf58b468a
steven vuong <stevenvuong@stevens-MacBook-Pro.local>
2020-09-06 21:52:31 +0100

    setup gcp

--- scripts/setup_tfvm.sh
+++ scripts/setup_tfvm.sh
@@ -10,3 +10,13 @@ gcloud compute instances create gpu-instance-deeplab \
     --maintenance-policy TERMINATE --restart-on-failure \
 
 gcloud compute firewall-rules create streamlit --allow tcp:8501 --source-tags="gpu-instance-deeplab" --source-ranges=0.0.0.0/0 --description="la"
+
+PROJECT_ID="fast-circle-288706"
+GCP_ZONE="europe-west4-c"
+CLUSTER_NAME="streamlit"
+
+gcloud auth login
+gcloud config set core/project ${PROJECT_ID}
+gcloud config set compute/zone ${GCP_ZONE}
+gcloud config set container/cluster ${CLUSTER_NAME}
+gcloud container clusters get-credentials ${CLUSTER_NAME}
