b622270b60ce8ecefc829ab258ce6dc58570c4e7
steven vuong <stevenvuong@stevens-MacBook-Pro.local>
2020-09-07 06:54:08 +0100

    deployed

--- scripts/setup_tfvm.sh
+++ scripts/setup_tfvm.sh
@@ -1,5 +1,5 @@
 set -e
-
+# https://towardsdatascience.com/deploy-machine-learning-app-built-using-streamlit-and-pycaret-on-google-kubernetes-engine-fd7e393d99cb
 gcloud config set project fast-circle-288706
 gcloud config set compute/zone europe-west4-c
 
@@ -15,8 +15,22 @@ PROJECT_ID="fast-circle-288706"
 GCP_ZONE="europe-west4-c"
 CLUSTER_NAME="streamlit"
 
+gcloud config list
+
 gcloud auth login
 gcloud config set core/project ${PROJECT_ID}
 gcloud config set compute/zone ${GCP_ZONE}
 gcloud config set container/cluster ${CLUSTER_NAME}
 gcloud container clusters get-credentials ${CLUSTER_NAME}
+
+gcloud projects add-iam-policy-binding "fast-circle-288706" --member="steven.vuong@foundersfactory.co" --role="roles/owner"
+
+
+gcloud builds submit --tag gcr.io/"fast-circle-288706"/app
+
+gcloud container clusters create ${CLUSTER_NAME} --num-nodes=2
+
+PROJECT_ID="fast-circle-288706"
+kubectl create deployment app --image=gcr.io/${PROJECT_ID}/app:v1
+kubectl expose deployment app --type=LoadBalancer --port 80 --target-port 8501
+kubectl get service
